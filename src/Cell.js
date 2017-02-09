5151b0d76d740bc0dd209be580a17b6952c5592a
Jan Schulte <hello@unexpected-co.de>
2017-02-09 21:00:54 +0100

    render readonly description cells for each row

--- src/Cell.js
+++ src/Cell.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './Cell.css';
 
 export default class Cell extends React.Component {
-  render() {
+  renderReadOnlyCaption() {
     return (
-      <div className="cell">
-        <input />
+      <span className="readonly">{this.props.caption}</span>
+    );
+  }
+
+  renderInputField() {
+    return (<input />);
+  }
+
+  render() {
+    const { isReadOnly } = this.props;
+
+    const cellClasses = isReadOnly ? "cell" : "cell";
+
+    return (
+      <div className={cellClasses}>
+        {isReadOnly ? this.renderReadOnlyCaption() : this.renderInputField()}
       </div>
     );
   }
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './Row.css';
+import Cell from './Cell';
 
 export default class Row extends React.Component {
+  descriptionCell() {
+    return (
+      <Cell
+        key={`description-A1`}
+        isReadOnly={true}
+        caption="A1"
+      >
+      </Cell>
+    );
+  }
+
   render() {
-    const { cells } = this.props;
+    const valueCells = this.props.cells;
+    const cells = [this.descriptionCell()].concat(valueCells);
+
     return (
       <div className="row">
         {cells}
