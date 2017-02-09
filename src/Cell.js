f3844ef4f6da8840cf338419098f22b38c5327cb
Jan Schulte <hello@unexpected-co.de>
2017-02-09 20:33:14 +0100

    render a single table cell

---
+++ src/Cell.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cell.css';
+
+export default class Cell extends React.Component {
+  render() {
+    return (
+      <div className="cell">
+        <input />
+      </div>
+    );
+  }
+}
