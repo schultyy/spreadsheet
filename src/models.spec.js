c411767e74a1154b6ab79f6667f5b38e6924b7a8
Jan Schulte <hello@unexpected-co.de>
2017-02-10 18:27:25 +0100

    Make command parser more resilient against (significant) whitespace

--- src/models.spec.js
+++ src/models.spec.js
@@ -1,27 +1,54 @@
 import { parseCommand, Spreadsheet } from './models';
 
 describe('parseCommand', () => {
-  const ast = parseCommand('A0 = A1 + A2');
+  describe('with whitespace separated', () => {
+    const ast = parseCommand('A0 = A1 + A2');
 
-  it('parses simple assignment', () => {
-    expect(ast.type).toEqual('assignment');
-  });
-  it('assigns correct target cell', () => {
-    expect(ast.target).toEqual('A0');
-  });
-  describe('expression', () => {
-    let expression = ast.expression;
-
-    it('has correct operation', () => {
-      expect(expression.operation).toEqual('+');
+    it('parses simple assignment', () => {
+      expect(ast.type).toEqual('assignment');
     });
-
-    it('has correct left child', () => {
-      expect(expression.left).toEqual('A1');
+    it('assigns correct target cell', () => {
+      expect(ast.target).toEqual('A0');
     });
+    describe('expression', () => {
+      let expression = ast.expression;
 
-    it('has correct right child', () => {
-      expect(expression.right).toEqual('A2');
+      it('has correct operation', () => {
+        expect(expression.operation).toEqual('+');
+      });
+
+      it('has correct left child', () => {
+        expect(expression.left).toEqual('A1');
+      });
+
+      it('has correct right child', () => {
+        expect(expression.right).toEqual('A2');
+      });
+    });
+  });
+  describe('without separating whitespace', () => {
+    const ast = parseCommand('A0=A1+A2');
+
+    it('parses simple assignment', () => {
+      expect(ast.type).toEqual('assignment');
+    });
+    it('assigns correct target cell', () => {
+      expect(ast.target).toEqual('A0');
+    });
+    describe('expression', () => {
+      let expression = ast.expression;
+
+      it('has correct operation', () => {
+        expect(expression.operation).toEqual('+');
+      });
+
+      it('has correct left child', () => {
+        expect(expression.left).toEqual('A1');
+      });
+
+      it('has correct right child', () => {
+        expect(expression.right).toEqual('A2');
+      });
     });
   });
 });
