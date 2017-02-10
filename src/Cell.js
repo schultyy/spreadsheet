5cf5620bbcecd33ad7d42f2690defa18af9df399
Jan Schulte <hello@unexpected-co.de>
2017-02-10 18:58:21 +0100

    Ensure that Cell only takes values after double Click

--- src/Cell.js
+++ src/Cell.js
@@ -10,7 +10,7 @@ export default class Cell extends React.Component {
   }
 
   renderInputField(onValueChange, value) {
-    return (<input onChange={onValueChange} value={value} />);
+    return (<CellContent onValueChange={onValueChange} value={value}></CellContent>);
   }
 
   render() {
@@ -21,6 +21,45 @@ export default class Cell extends React.Component {
     return (
       <div className={cellClasses}>
         {isReadOnly ? this.renderReadOnlyCaption() : this.renderInputField(onValueChange, value)}
+      </div>
+    );
+  }
+}
+
+class CellContent extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isActive: false
+    };
+  }
+
+  onMouseDoubleClick(event) {
+    this.setState({ isActive: true });
+  }
+
+  onInputKeyDown(event) {
+    if(event.keyCode === 13) {
+      this.setState({ isActive: false });
+    }
+  }
+
+  renderInputField(value) {
+    const { onValueChange } = this.props;
+
+    return (<input value={value} onKeyDown={this.onInputKeyDown.bind(this)} onChange={onValueChange} />);
+  }
+
+  render() {
+    const { value } = this.props;
+    const { isActive } = this.state;
+
+    return (
+      <div onDoubleClick={this.onMouseDoubleClick.bind(this)}>
+        { isActive ?
+          this.renderInputField(value) :
+          value }
       </div>
     );
   }
