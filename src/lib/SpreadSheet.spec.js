47e9cb5519429775469321d827d83e8084c84dc1
Jan Schulte <hello@unexpected-co.de>
2017-02-10 22:14:23 +0100

    move command parser code into its own module

--- src/lib/SpreadSheet.spec.js
+++ src/lib/SpreadSheet.spec.js
@@ -1,4 +1,4 @@
-import { SpreadSheet, parseCommand } from './SpreadSheet';
+import { SpreadSheet } from './SpreadSheet';
 import { Row, Cell } from './models';
 
 describe('SpreadSheet', () => {
@@ -35,82 +35,3 @@ describe('SpreadSheet', () => {
   });
 });
 
-describe('parseCommand', () => {
-  describe('simple value assignment', () => {
-    context('with valid value', () => {
-      const ast = parseCommand('A0 = 5');
-
-      describe('ast', () => {
-        it('has type assignment', () => {
-          expect(ast.type).toEqual('assignment');
-        });
-
-        describe('expression', () => {
-          it('has type "value"', () => {
-            expect(ast.expression.type).toEqual('value');
-          });
-
-          it('has correct value', () => {
-            expect(ast.expression.value).toEqual(5);
-          });
-        });
-      });
-    });
-    it ('raises error when value is invalid', () => {
-      expect(() => {
-        parseCommand('A0 = BFS');
-      }).toThrow();
-    });
-  });
-  describe('with whitespace separated', () => {
-    const ast = parseCommand('A0 = A1 + A2');
-
-    it('parses simple assignment', () => {
-      expect(ast.type).toEqual('assignment');
-    });
-    it('assigns correct target cell', () => {
-      expect(ast.target).toEqual('A0');
-    });
-    describe('expression', () => {
-      let expression = ast.expression;
-
-      it('has correct operation', () => {
-        expect(expression.operation).toEqual('+');
-      });
-
-      it('has correct left child', () => {
-        expect(expression.left).toEqual('A1');
-      });
-
-      it('has correct right child', () => {
-        expect(expression.right).toEqual('A2');
-      });
-    });
-  });
-
-  describe('without separating whitespace', () => {
-    const ast = parseCommand('A0=A1+A2');
-
-    it('parses simple assignment', () => {
-      expect(ast.type).toEqual('assignment');
-    });
-    it('assigns correct target cell', () => {
-      expect(ast.target).toEqual('A0');
-    });
-    describe('expression', () => {
-      let expression = ast.expression;
-
-      it('has correct operation', () => {
-        expect(expression.operation).toEqual('+');
-      });
-
-      it('has correct left child', () => {
-        expect(expression.left).toEqual('A1');
-      });
-
-      it('has correct right child', () => {
-        expect(expression.right).toEqual('A2');
-      });
-    });
-  });
-});
---
+++ src/lib/commandParser.js
@@ -0,0 +1,90 @@
+
+export function parseCommand(commandString) {
+  //A0 = A1 + A2
+  //assignmentAST[0] -> target cell
+  //assignmentAST[1] -> expression
+  const assignmentAST = commandString.trim().split('=');
+
+  if(assignmentAST.length !== 2) {
+    throw new Error(`Invalid syntax. Expected: A0 = A1 + A2, Got: ${commandString}`);
+  }
+  const targetCellName = assignmentAST[0].trim();
+  const expression = assignmentAST[1].trim();
+  const operatorIndex = getOperatorIndex(expression);
+  var expressionAST = null;
+
+  if (operatorIndex !== -1 && !isNumber(expression)) {
+    //Looks like we encountered an equation
+    expressionAST = equation(operatorIndex, expression);
+  } else if (operatorIndex === -1 && isNumber(expression)) {
+    //Looks like we encountered a simple value
+    expressionAST = {
+      type: 'value',
+      value: parseInt(expression, 10)
+    };
+  }
+  else {
+    throw new Error(`Expected a number or expression, got: ${expression}`);
+  }
+
+  return {
+    type: 'assignment',
+    target: targetCellName,
+    expression: expressionAST
+  };
+};
+
+function equation(operatorIndex, expression) {
+  const operator = expression[operatorIndex].trim();
+  const left = expression.substring(0, operatorIndex).trim();
+  const right = expression.substring(operatorIndex + 1).trim();
+
+  assertRequiredValue(operator, ['-', '+', '*', '/']);
+  assertRequiredNotNull(left, 'Left Operand');
+  assertRequiredNotNull(right, 'Right Operand');
+
+  return {
+    operation: operator,
+    left: left,
+    right: right
+  };
+}
+
+function isNumber(potentialNumber) {
+  return !isNaN(potentialNumber);
+}
+
+function assertRequiredValue(stringToTest, expected) {
+  if (expected.constructor === Array) {
+    if(!expected.some(e => stringToTest === e)) {
+      throw new Error(`Expected one of ${expected}, got ${stringToTest}`);
+    }
+  } else {
+    if(expected === stringToTest) {
+      return;
+    }
+    throw new Error(`Expected ${expected}, got ${stringToTest}`);
+  }
+}
+
+function assertRequiredNotNull(stringToTest, expectation) {
+  if (typeof stringToTest === 'undefined' || stringToTest === null) {
+    throw new Error(`Expected ${expectation} to not be null or undefined`);
+  }
+}
+
+function getOperatorIndex(expressionStr) {
+  const operators = ['+', '-', '/', '*'];
+
+  const operatorMapping = operators.map((op) => {
+    return { operator: op, index: expressionStr.indexOf(op) };
+  })
+  .filter((op) => op.index !== -1);
+
+  if (operatorMapping.length > 0) {
+    return operatorMapping[0].index;
+  }
+  else {
+    return -1;
+  }
+}
