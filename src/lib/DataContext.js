e0449e2bd1d144c5e2f600716382d27b5742f8db
Jan Schulte <hello@unexpected-co.de>
2017-02-12 22:10:58 +0100

    Use a real SpreadSheet instead of a mock
    We need to work with a real spreadsheet eventually anyways so let's save
    the time setting up mocks

--- src/lib/DataContext.js
+++ src/lib/DataContext.js
@@ -4,3 +4,11 @@ export default function DataContext(spreadsheet) {
   this.spreadsheet = spreadsheet;
   this.pouch = new PouchDB(this.spreadsheet.filename);
 }
+
+DataContext.prototype.eval = function(args) {
+  this.spreadsheet.eval(args);
+};
+
+DataContext.prototype.addFormulaToCell = function(args) {
+  this.spreadsheet.addFormulaToCell(args);
+};
--- src/lib/DataContext.spec.js
+++ src/lib/DataContext.spec.js
@@ -1,31 +1,46 @@
 import PouchDB from 'pouchdb';
 import DataContext from './DataContext';
 import fs from 'fs-extra';
+import { SpreadSheet } from './SpreadSheet';
 
 const DBName = 'testsheet';
 
 afterAll(() => {
   //Clean up PouchDB test database
   fs.removeSync(DBName);
-})
+});
 
 describe('DataContext', () => {
-  describe('.new', () => {
-    const spreadSheetMock = {
-      filename: DBName
-    };
-    const context = new DataContext(spreadSheetMock);
+  const spreadsheet = new SpreadSheet();
+  spreadsheet.setFilename(DBName);
 
+  const context = new DataContext(spreadsheet);
+  describe('.new', () => {
     it('initializes a new PouchDB instance', () =>{
       expect(context.pouch.constructor).toEqual(PouchDB);
     });
 
     it('assigns spreadsheet', () => {
-      expect(context.spreadsheet).toEqual(spreadSheetMock);
+      expect(context.spreadsheet).toEqual(spreadsheet);
     });
 
     it("creates a database with the spreadsheet's name", () => {
-      expect(context.pouch.name).toEqual(spreadSheetMock.filename);
+      expect(context.pouch.name).toEqual(DBName);
+    });
+  });
+
+  describe('.eval', () => {
+    it("delegates to spreadsheet's eval", () => {
+      context.eval('A0 = 5');
+      expect(spreadsheet.rows[0].cells[0].value).toEqual(5);
+    });
+  });
+
+  describe('.addFormulaToCell', () => {
+    it("delegates to spreadsheet's eval", () => {
+      const formula = 'A0 = A1 + A2';
+      context.addFormulaToCell(formula);
+      expect(spreadsheet.rows[0].cells[0].formula).toEqual(formula);
     });
   });
 });
