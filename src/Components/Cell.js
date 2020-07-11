70ac5ffbe2e4699f9793134298d5eecda7109f9a
Jan Schulte <jan.schulte@hey.com>
2020-07-11 18:12:49 -0400

    Render readonly captions

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -9,73 +9,12 @@ export default class Cell extends React.Component {
     );
   }
 
-  renderInputField(onValueChange, hasFormula, value) {
-    return (<CellContent onValueChange={onValueChange} hasFormula={hasFormula} value={value}></CellContent>);
-  }
-
   render() {
-    const { isReadOnly, onValueChange, hasFormula, value } = this.props;
-
-    const cellClasses = isReadOnly ? "cell" : "cell";
+    const { isReadOnly } = this.props;
 
     return (
-      <div className={cellClasses}>
-        {isReadOnly ? this.renderReadOnlyCaption() : this.renderInputField(onValueChange, hasFormula, value)}
-      </div>
-    );
-  }
-}
-
-class CellContent extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      isActive: false
-    };
-  }
-
-  onMouseClick() {
-    const { hasFormula } = this.props;
-    if(!hasFormula) {
-      this.setState({ isActive: true });
-    }
-  }
-
-  onInputKeyDown(event) {
-    if(event.keyCode === 13) {
-      this.setState({ isActive: false });
-    }
-  }
-
-  onInputBlur(event) {
-    this.setState({ isActive: false });
-  }
-
-  renderInputField(value) {
-    const { onValueChange } = this.props;
-
-    return (
-      <input autoFocus value={value}
-             onBlur={this.onInputBlur.bind(this)}
-             onKeyDown={this.onInputKeyDown.bind(this)}
-             onChange={onValueChange}
-      />
-    );
-  }
-
-  render() {
-    const { value, hasFormula } = this.props;
-    const { isActive } = this.state;
-
-    const classNames = hasFormula ? "readonly" : "";
-
-    return (
-      <div className={classNames} onClick={this.onMouseClick.bind(this)}>
-        { isActive ?
-          this.renderInputField(value) :
-          <span className="content">{value}</span>
-        }
+      <div className="cell">
+        {isReadOnly ? this.renderReadOnlyCaption() : 0}
       </div>
     );
   }
