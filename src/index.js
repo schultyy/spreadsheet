f43a395f5494601a673e66891457b5af36103166
Jan Schulte <jan.schulte@hey.com>
2020-07-12 11:49:58 -0400

    Use App Container

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './Components/App';
+import App from './Containers/App';
 import configureStore from './configureStore';
 import './index.css';
 
